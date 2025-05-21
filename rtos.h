4ef2fe5fea7da1c692b03dcde5d3857986c4f44b
kmkimmmm <andyk0719@gmail.com>
2025-05-21 19:46:17 +0900

    주석 및 구현 완료

--- rtos.h
+++ rtos.h
@@ -16,10 +16,10 @@ typedef struct
 } Task;
 
 // 전역 변수 (rtos_scheduler.c에서 정의)
-extern Task tasks[MAX_TASKS];       // Task 구조체를 담는 배열
-extern int task_count ;          // 배열에 저장된 task의 수
-extern volatile uint64_t ticks ; // 1ms가 몇 번 지났는가? = tick
-extern volatile int yield_flag ; // 선점 요청 정보를 담는 변수
+extern Task tasks[MAX_TASKS];   // Task 구조체를 담는 배열
+extern int task_count;          // 배열에 저장된 task의 수
+extern volatile uint64_t ticks; // 1ms가 몇 번 지났는가? = tick
+extern volatile int yield_flag; // 선점 요청 정보를 담는 변수
 
 // 스케쥴러에서 사용할 함수
 void register_task(int period_ms, void (*f)(void));
