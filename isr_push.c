23107e516590be6c1fdbd8567b00cf2e9419c1cc
Jimin <jmhwang236@gmail.com>
2025-05-28 13:49:34 +0900

    isr_push grammar fixed

--- isr_push.c
+++ isr_push.c
@@ -9,8 +9,8 @@
 #include <stdint.h>
 
 extern int fd_push; // device_io.c (재훈) 에서 정의한 push 관련 파일 디스크립터
-extern volatile int yield_flag; // 스케쥴러 선점 요청 플래그 (규민이에게 전달)
-extern volatile int buzzer_flag; // 버저 요청 플래그 (성빈이에게 전달)
+volatile int yield_flag = 0;  // 스케쥴러 선점 요청 플래그 (규민이에게 전달)
+volatile int buzzer_flag = 0; // 버저 요청 플래그 (성빈이에게 전달)
 
 static uint8_t led_state = 0; // LED 토글 상태를 관리하기 위한 변수
 
